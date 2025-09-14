1187a4d0b9cf445735dcd7377a45a17cc6522cbe
niraj-verma07 <vermaniraj9607@gmail.com>
2025-09-14 17:00:33 +0530

    Request and Response

--- Express-Js/index.js
+++ Express-Js/index.js
@@ -5,3 +5,16 @@ let port = 3000;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
+
+//Sending request to server
+app.use((req, res) => {
+  console.log("New Request recived");
+  //Sending response to the user
+  //   res.send("This is the response"); //Normal text message
+  //   res.send({ // Object
+  //     name: "Niraj",
+  //     OS: "Windows",
+  //   });
+
+  res.send("<h1>HTML tag</h1>"); //Html tag
+});
