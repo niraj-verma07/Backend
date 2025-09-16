0d34df33aad584788b4a8a19210b0e036f78a165
niraj-verma07 <vermaniraj9607@gmail.com>
2025-09-16 14:02:56 +0530

    Query String

--- Express-Js/index.js
+++ Express-Js/index.js
@@ -45,13 +45,26 @@ app.post("/", (req, res) => {
 // });
 
 //Path parameters
-app.get("/:username", (req, res) => {
-  console.log(req.params);
-  res.send("Username response");
-});
+// app.get("/:username", (req, res) => {
+//   console.log(req.params);
+//   res.send("Username response");
+// });
 
 app.get("/:username/:id", (req, res) => {
   let { username, id } = req.params; //object destruture
   console.log(req.params); //http://localhost:3000/niraj/123
   res.send(`${username} with ${id} response`);
+});
+
+// app.get("/search", (req, res) => {
+//   console.log(req.query);
+//   res.send("no results");
+// });
+
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  if (!q) {
+    res.send("No such query");
+  }
+  res.send("no results");
 });
