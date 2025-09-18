dcb789d61a44f673bcc46b737dabb0ab9f995e99
niraj-verma07 <vermaniraj9607@gmail.com>
2025-09-18 12:57:14 +0530

    Loops in ejs

--- EJS/index.js
+++ EJS/index.js
@@ -27,9 +27,10 @@ app.get("/rolldice", (req, res) => {
 // Instagram EJS
 
 app.get("/ig/:username", (req, res) => {
+  const followers = ["adam", "blob", "steve", "abc"];
   let { username } = req.params;
   // console.log(username);
-  res.render("instagram.ejs", { username });
+  res.render("instagram.ejs", { username, followers });
 });
 
 app.listen(port, () => {
