661c5f75ba382731d4c05fcd342d9c0019b39ce1
Niraj Verma <vermaniraj9607@gmail.com>
2025-11-18 12:02:20 +0530

    Node js

---
+++ Node-Js/Fruits/index.js
@@ -0,0 +1,7 @@
+let apple = require("./apple");
+let mango = require("./mango");
+let orange = require("./orange");
+
+let fruits = [apple, mango, orange];
+
+module.exports = fruits;
--- Node-Js/script.js
+++ Node-Js/script.js
@@ -18,3 +18,7 @@ const value = require("./math");
 
 console.log(value);
 // console.log(value.sum(10, 20)); //Use of required function
+
+//Exporting Directory (make a index.js file in that directory and require all the files there and export index.js file and require that directory in main file)
+let info = require("./Fruits");
+console.log(info);
